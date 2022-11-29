b78b1d8f07fafa7a3d8c93ee46bfb617604cf835
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2022-11-29 16:05:55 -0600

    Corrigiendo Slider

--- src/componentes/Informacion.js
+++ src/componentes/Informacion.js
@@ -99,39 +99,6 @@ const Informacion = () => {
                 </div>
             </InformacionContenedor2>
 
-            <Slider className='row mt-4 justify-content-center'>
-                
-                    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <div className='col-11 contenedor'>
-                                    <img src={Slider1} 
-                                        alt="Tarjet | Tu tarjeta de presentación Online" 
-                                        className='img-fluid'
-                                    />
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <div className='col-11 contenedor'>
-                                    <img src={Slider2} 
-                                        alt="Tarjet | Tu tarjeta de presentación Online" 
-                                        className='img-fluid'
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        {/* <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Next</span>
-                        </a> */}
-                    </div>
-                
-            </Slider>
-
             <InformacionContenedor className='row mt-4 justify-content-center'>
                 <div className='col-11 contenedor'>
                     <p>
@@ -161,6 +128,39 @@ const Informacion = () => {
                     </motion.button>
                 </div>
             </InformacionContenedor>
+
+            <Slider className='row mt-4 justify-content-center'>
+                
+                    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <div className='col-11 contenedor'>
+                                    <img src={Slider1} 
+                                        alt="Tarjet | Tu tarjeta de presentación Online" 
+                                        className='img-fluid'
+                                    />
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <div className='col-11 contenedor'>
+                                    <img src={Slider2} 
+                                        alt="Tarjet | Tu tarjeta de presentación Online" 
+                                        className='img-fluid'
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            {/* <span className="carousel-control-prev-icon" aria-hidden="true"></span> */}
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            {/* <span className="carousel-control-next-icon" aria-hidden="true"></span> */}
+                            <span className="sr-only">Next</span>
+                        </a>
+                    </div>
+                
+            </Slider>
         </>
     );
 }
